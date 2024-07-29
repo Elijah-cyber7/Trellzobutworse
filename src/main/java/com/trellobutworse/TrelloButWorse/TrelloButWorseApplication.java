c9f419d6767894e220fcb2bb9c602a8857f90b1e
Andrew Allen <63973997+elijah-cyber7@users.noreply.github.com>
2024-07-29 15:34:31 -0500

    Initial commit

---
+++ src/main/java/com/trellobutworse/TrelloButWorse/TrelloButWorseApplication.java
@@ -0,0 +1,13 @@
+package com.trellobutworse.TrelloButWorse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrelloButWorseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrelloButWorseApplication.class, args);
+	}
+
+}
